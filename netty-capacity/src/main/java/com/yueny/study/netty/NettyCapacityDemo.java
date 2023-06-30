c5367677926df64754a8d007a1329ccd7a47c736
fengyang <yueny09@163.com>
2023-06-30 19:21:16 +0800

    demo

--- netty-capacity/src/main/java/com/yueny/study/netty/NettyCapacityDemo.java
+++ netty-capacity/src/main/java/com/yueny/study/netty/NettyCapacityDemo.java
@@ -13,24 +13,32 @@ import java.util.Arrays;
 public class NettyCapacityDemo {
     public static void main(String[] args) {
         // 1.创建一个非池化的ByteBuf，大小为10个字节
-        ByteBuf buf = Unpooled.buffer(10);
+        ByteBuf buf = Unpooled.buffer(30);
         System.out.println("创建一个非池化的ByteBuf, 大小为10个字节");
         System.out.println("原始ByteBuf为====================>"+buf.toString());
         System.out.println("1.ByteBuf中的内容为===============>"+ Arrays.toString(buf.array())+"\n");
 
         // 2.写入一段内容
-        System.out.println("写入一段内容");
-        byte[] bytes = {1,2,3,4,5};
+        System.out.println("写入20");
+        byte[] bytes = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         buf.writeBytes(bytes);
         System.out.println("写入的bytes为====================>"+Arrays.toString(bytes));
         System.out.println("写入一段内容后ByteBuf为===========>"+buf.toString());
         System.out.println("2.ByteBuf中的内容为===============>"+Arrays.toString(buf.array())+"\n");
 
         // 3.读取一段内容
-        System.out.println("读取一段内容");
+        System.out.println("读取10");
         byte b1 = buf.readByte();
         byte b2 = buf.readByte();
-        System.out.println("读取的bytes为====================>"+Arrays.toString(new byte[]{b1,b2}));
+        byte b3 = buf.readByte();
+        byte b4 = buf.readByte();
+        byte b5 = buf.readByte();
+        byte b6 = buf.readByte();
+        byte b7 = buf.readByte();
+        byte b8 = buf.readByte();
+        byte b9 = buf.readByte();
+        byte b10 = buf.readByte();
+        System.out.println("读取的bytes为====================>"+Arrays.toString(new byte[]{b1,b2,b3,b4,b5,b6,b7,b8,b9,b10}));
         System.out.println("读取一段内容后ByteBuf为===========>"+buf.toString());
         System.out.println("3.ByteBuf中的内容为===============>"+Arrays.toString(buf.array())+"\n");
 
@@ -62,7 +70,7 @@ public class NettyCapacityDemo {
 
         // 8.再次写入一段超过容量的内容
         System.out.println("再写，出现扩容");
-        byte[] bytes3 = {1,2,3,4,5,6,7,8,9,10,11};
+        byte[] bytes3 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
         buf.writeBytes(bytes3);
         System.out.println("写入的bytes为====================>"+Arrays.toString(bytes3));
         System.out.println("写入一段内容后ByteBuf为===========>"+buf.toString());
